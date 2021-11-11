d9734e2bff836e983c35ff0c8947bdbc671a1baa
adrianlongoria <strongmachine@icloud.com>
2021-11-11 07:38:14 -0600

    homework

--- static/js/logic.js
+++ static/js/logic.js
@@ -72,7 +72,7 @@ function createMap(earthquakes) {
     });
 
     var Satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> , Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
         id: "satellite-v9",
         accessToken: API_KEY
